468b58380414c85ec95799612b3914035c813a17
subrat1516 <160724970+subrat1516@users.noreply.github.com>
2024-02-21 16:54:26 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,21 @@
+class bapi
+{
+	public static void main(String arg[])
+	{
+		int a=10;
+		if (a<0)
+		{
+			System.out.println("negetive number");
+		
+		}
+		else
+		{
+			do
+			{
+				System.out.println(a);
+				a--;
+			}
+			while (a>1);
+		}
+	}
+}
